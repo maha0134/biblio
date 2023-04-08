480801d4347a0510e278f8e0de3723c7b2e8b418
Akshay Mahajan <maha0134@algonquinlive.com>
2023-04-08 13:24:29 -0400

    Initial Commit

---
+++ Biblio/BiblioApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiblioApp.swift
+//  Biblio
+//
+//  Created by AKSHAY MAHAJAN on 2023-04-08.
+//
+
+import SwiftUI
+
+@main
+struct BiblioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
